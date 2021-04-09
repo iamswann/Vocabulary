1338e8bcbfb69b80bbc8415e1f6394d0b9872d57
quannguyen569 <ngquan569@gmail.com>
2021-04-09 13:04:55 +0700

    add

---
+++ __manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "vocabulary",
+
+    'summary': """
+        Quản lý học từ vựng""",
+
+    'description': """
+        Ứng dụng quản lý việc học từ vựng
+    """,
+
+    'author': "Nguyen Anh Quan",
+
+    'category': 'Education',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/words.xml',
+        'views/meaning.xml',
+        'views/example.xml',
+        'views/test.xml',
+        'views/quiz.xml',
+        'views/category.xml',
+        'views/student.xml',
+        'views/learn_section.xml',
+    ],
+    'application': True,
+    'installable': True,
+}
